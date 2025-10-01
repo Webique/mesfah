3fbcecf534fe805383f482699d2f0a196d1d69c0
Adham Tarek <adhamtarek291@gmail.com>
2025-10-01 14:19:30 +0300

    first commit

---
+++ src/app/[locale]/(landing)/page.tsx
@@ -0,0 +1,19 @@
+import type { Locale } from "next-intl";
+
+import { setRequestLocale } from "next-intl/server";
+import { use } from "react";
+
+import WhatsAppFloat from "@/components/whats-app-float";
+
+export default function IndexPage({ params }: PageProps<"/[locale]">) {
+  const { locale } = use(params);
+
+  // Enable static rendering
+  setRequestLocale(locale as Locale);
+
+  return (
+    <main>
+      <WhatsAppFloat />
+    </main>
+  );
+}
